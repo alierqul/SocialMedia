bd007bed9a0429a2c6cd655ad66df6d2f7ff391a
Ali ERGÜL <alierqul@hotmail.com>
2022-02-15 13:04:27 +0300

    api-gateway modülü eklendi.

---
+++ service-auth/src/main/java/com/aliergul/auth/constant/RestApiUrl.java
@@ -0,0 +1,33 @@
+package com.aliergul.auth.constant;
+
+public class RestApiUrl {
+    /**
+     * Resp Api Servislerinizin versiyonlarını tutmak için kullanın.
+     */
+    public static final String VERSION = "/v1";
+
+    /**
+     * CONTOLLER kısımlarında ki map listesini ayrıca burada yazalım
+     */
+    public static final String PROFILE = "/profile";
+    public static final String AUTH = "/auth";
+
+
+    /**
+     * GENEL olarak tanımlanan isteklerin burada tanımlayalım
+     */
+    public static final String SAVE= "/save";
+    public static final String UPDATE= "/update";
+    public static final String DELETE= "/delete";
+    public static final String FINDALL= "/findall";
+    public static final String GETALL= "/getall";
+    public static final String FINDBYID= "/findbyid";
+    public static final String FINDBYNAME= "/findbyname";
+
+    /**
+     * CONTROLLER için özel olarak tanımladığınız istek url lerini burada tanımlayalım ayrıştırabilirsiniz.
+     */
+    public static final String DOLOGIN="/dologin";
+    public static final String DOSIGNUP="/signup";
+    public static final String FIND_BY_AUTH_ID= "/findbyauthid";
+}
